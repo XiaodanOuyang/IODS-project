daca41d68341e2db93625bc18d9f34287ef71277
unknown <you@example.com>
2020-11-29 19:32:37 +0200

    revised

--- data/create_human.R
+++ data/create_human.R
@@ -1,25 +1,28 @@
 Xiaodan Ouyang
 human<-read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt")
+names(human)
 str(human)
 dim(human)
 library(stringr)
 str(human$GNI)
 str_replace(human$GNI, pattern=",", replace ="")%>%as.numeric
+library(dplyr)
 keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
 human <- select(human, one_of(keep))
 str(human)
 complete.cases(human)
 data.frame(human[-1], comp = complete.cases(human))
 human_ <- filter(human, complete.cases(human))
+human<-human_
 tail(human, 10)
 last <- nrow(human) - 7
 human_ <- human[1:last, ]
-rownames(human) <- human$Country
-library(GGally)
-dplyr::select()
+rownames(human_) <- human_$Country
 library(corrplot)
-human_ <- select(human, -Country)
-str(human_)
-ggpairs(human_)
-
-
+human <- select(human_,-Country)
+library(GGally)
+str(human)
+human%>% mutate_all(as.numeric) %>%
+ggpairs(human)
+write.csv(human,file ="data/human.csv",row.names = FALSE)
+human<-read.csv(file = "data/human.csv")
